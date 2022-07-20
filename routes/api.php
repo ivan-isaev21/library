e7d1a80bc69b2badf3a515fdcbb2f819770be851
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-07-20 16:11:25 +0300

    Create book api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'v1/external',
+    'namespace'  => 'App\Http\Controllers\Api\v1\External'
+], function () {
+    Route::apiResource('books', BookController::class);
+});
