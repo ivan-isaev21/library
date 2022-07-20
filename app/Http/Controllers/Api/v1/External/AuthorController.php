19a1065860b937675c733eaf3c40a8d426a09e5b
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-07-20 16:49:21 +0300

    Create author api controller

---
+++ app/Http/Controllers/Api/v1/External/AuthorController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\External;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\AuthorResource;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        return AuthorResource::collection(Author::paginate(10));
+    }
+
+    public function store(\App\Http\Requests\CreateAuthorRequest $request)
+    {
+        $createdAuthor = Author::create($request->only(['first_name', 'last_name']));
+        return response(new AuthorResource($createdAuthor), Response::HTTP_CREATED);
+    }
+
+    public function show(Author $author)
+    {
+        return new AuthorResource($author);
+    }
+
+    public function update(\App\Http\Requests\UpdateAuthorRequest $request, Author $author)
+    {
+        $author->update($request->only(['first_name', 'last_name']));
+        return response(new AuthorResource($author), Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy(Author $author)
+    {
+        $author->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
