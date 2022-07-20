e6287f61f5b38095071a4935772ee30fd5f5c31c
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-07-20 16:10:55 +0300

    Create book api controller

---
+++ app/Http/Controllers/Api/v1/External/BookController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\External;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BookResource;
+use Illuminate\Http\Request;
+use App\Models\Book;
+use Symfony\Component\HttpFoundation\Response;
+use App\Services\BookService;
+
+class BookController extends Controller
+{
+    public $bookService;
+
+    public function __construct(BookService $bookService)
+    {
+        $this->bookService = $bookService;
+    }
+
+    public function index()
+    {
+        return BookResource::collection(Book::paginate(10));
+    }
+
+    public function store(\App\Http\Requests\CreateBookRequest $request)
+    {
+        $createdBook = $this->bookService->create($request);
+        return response(new BookResource($createdBook), Response::HTTP_CREATED);
+    }
+
+    public function show(Book $book)
+    {
+        return new BookResource($book);
+    }
+
+    public function update(\App\Http\Requests\UpdateBookRequest $request, Book $book)
+    {
+        $updatedBook = $this->bookService->update($request, $book);
+        return response(new BookResource($updatedBook), Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
