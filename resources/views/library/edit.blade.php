fe26bc2b1b80efc539e74c878baaa6f3fa40db5b
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-07-21 21:48:18 +0300

    Change edit book view

--- resources/views/library/edit.blade.php
+++ resources/views/library/edit.blade.php
@@ -3,12 +3,21 @@
 @section('content')
 <div class="album py-5 bg-light">
     <div class="container">
+
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <form>
+
+                <div id="success" class="d-none">
+                    <x-alert type="success" message="Success update book!" />
+                </div>
+
+                <form id="bookForm">
                     <div class="form-group" id="titleWrap">
                         <label for="title">Title</label>
-                        <input type="text" class="form-control" id="title">
+                        <input type="text" class="form-control" id="title" name="title">
+                        <div id="validationServerTitleFeedback" class="invalid-feedback">
+
+                        </div>
                     </div>
 
                     <div class="form-group" id="authorsWrap">
@@ -30,11 +39,10 @@
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 <script defer>
     $(document).ready(function() {
-        loadBook("{{$book->id}}");
-        //loadAuthors();
+        loadBookForm("{{$book->id}}");
     });
 
-    function loadBook(id) {
+    function loadBookForm(id) {
         $.ajax({
             type: "GET",
             url: base_url + 'api/v1/external/books/' + id,
@@ -71,17 +79,16 @@
     }
 
     function setAuthors(authors, book) {
-
         let html = '<label for="authors">Authors</label>';
-        html += '<select class="form-control" id="authors" name="authors" multiple>';
+        html += '<select class="form-control" id="authors" name="authors[]" multiple>';
 
         for (author of authors) {
             let selected = null;
 
             for (bookAuthor of book.authors) {
                 selected = null
-                
-                if (bookAuthor.id == author.id) {                   
+
+                if (bookAuthor.id == author.id) {
                     selected = 'selected';
                     break;
                 }
@@ -94,26 +101,57 @@
     }
 
     function setPublishers(publishers, book) {
-                
         let html = '<label for="publishers">Publishers</label>';
-        html += '<select class="form-control" id="publishers" name="publishers" multiple>';
+        html += '<select class="form-control" id="publishers" name="publishers[]" multiple>';
 
         for (publisher of publishers) {
             let selected = null;
 
             for (bookPublisher of book.publishers) {
                 selected = null
-                
+
                 if (bookPublisher.id == publisher.id) {
                     selected = 'selected';
                     break;
                 }
             }
-            
+
             html += `<option value="${publisher.id}" ${selected} >${publisher.name}</option>`;
         }
         html += '</select>';
         $('#publishersWrap').html(html);
+    }
+
+    $('#bookForm').on('submit', e => {
+        e.preventDefault();
+
+        $.ajax({
+            type: "PUT",
+            url: base_url + 'api/v1/external/books/{{$book->id}}',
+            data: $('#bookForm').serialize(),
+            success: response => {
+                this.clearClientErrors();
+                this.displaySuccessAlert();
+                console.log(response);
+            },
+            error: response => {
+                if (response.responseJSON.errors && response.responseJSON.errors.hasOwnProperty('title')) {
+                    $('#title').addClass('is-invalid');
+                    $('#validationServerTitleFeedback').html(response.responseJSON.errors.title);
+                }
+            }
+        });
+
+    });
+
+    function displaySuccessAlert() {
+        $('#success').removeClass('d-none');
+    }
+
+    function clearClientErrors() {
+        $('.is-invalid').each(function(index, e) {
+            $(this).removeClass('is-invalid');
+        });
     }
 </script>
 
