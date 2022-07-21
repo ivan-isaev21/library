ea1d5ad646fb03f0d0a6920c549039f9c3f0aa1e
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-07-21 19:47:14 +0300

    Add library controller

---
+++ app/Http/Controllers/Library/LibraryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Library;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Http\Resources\BookResource;
+
+class LibraryController extends Controller
+{
+    public function index(){       
+        return view("library.index");
+    }
+
+    public function edit(Book $book){
+        return view("library.edit", compact('book'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\Library\LibraryController::class, 'index'])->name('home');
+Route::get('/books/{book}/edit', [\App\Http\Controllers\Library\LibraryController::class, 'edit']);
 
 Auth::routes();
 
