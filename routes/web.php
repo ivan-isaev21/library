599d6b89360f83b35ccf9afc2fd442ff8fa3e86b
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-07-21 14:05:41 +0300

    Change home route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\Library\LibraryController::class, 'index'])->name('home');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
