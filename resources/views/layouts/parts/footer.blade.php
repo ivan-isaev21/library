4b5c2ef44b01ce7b160e2709ae678fcf0cb328ba
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-07-21 14:04:49 +0300

    Add footer part

---
+++ resources/views/layouts/parts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+        © Ivan Isaev
+        </p>
+        <p>Library test task for work  </p>        
+    </div>
+</footer>
